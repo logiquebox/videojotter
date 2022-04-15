f3fd14540cedacffb7e1cb24c38be1e8ae9d5514
Ujah Emmanuel <ujahemmanuel72@gmail.com>
2022-04-15 10:24:51 +0100

    implement CRUD operations

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const methodOverride = require("method-override");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
+const flash = require("connect-flash");
+const session = require("express-session");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -22,12 +25,44 @@ require("./models/Idea");
 const Idea = mongoose.model("ideas");
 
 // Handlebars Middleware
-app.engine("handlebars", exphbs.engine());
+app.engine(
+  "handlebars",
+  exphbs.engine({
+    defaultLayout: "main",
+    runtimeOptions: {
+      allowProtoPropertiesByDefault: true,
+      allowProtoMethodsByDefault: true,
+    },
+  })
+);
 app.set("view engine", "handlebars");
 
 // Body Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// methodOverride Middleware
+app.use(methodOverride("_method"));
+
+// Express Session Middleware
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+// Flash Middleware
+app.use(flash());
+
+// Global variable
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 // Index page
 app.get("/", (req, res) => {
@@ -35,9 +70,31 @@ app.get("/", (req, res) => {
   res.render("index", { title: title });
 });
 
+// Idea Index Page
+app.get("/ideas", (req, res) => {
+  Idea.find({})
+    .sort({ date: "desc" })
+    .then((ideas) => {
+      res.render("ideas/index", {
+        ideas: ideas,
+      });
+    });
+});
+
 // Add Idea page
 app.get("/ideas/add", (req, res) => {
   res.render("ideas/add");
+});
+
+// Edit idea page
+app.get("/ideas/edit/:id", (req, res) => {
+  Idea.findOne({
+    _id: req.params.id,
+  }).then((idea) => {
+    res.render("ideas/edit", {
+      idea: idea,
+    });
+  });
 });
 
 // Process form data
@@ -56,8 +113,39 @@ app.post("/ideas", (req, res) => {
       details: req.body.details,
     });
   } else {
-    res.send("Success");
+    const newIdea = {
+      title: req.body.title,
+      details: req.body.details,
+    };
+    new Idea(newIdea).save().then((idea) => {
+      req.flash("success_msg", "Video idea Added!");
+      res.redirect("/ideas");
+    });
   }
+});
+
+// Process form edit
+app.put("/ideas/:id", (req, res) => {
+  Idea.findOne({
+    _id: req.params.id,
+  }).then((idea) => {
+    idea.title = req.body.title;
+    idea.details = req.body.details;
+    idea.save().then((idea) => {
+      req.flash("success_msg", "Video idea Updated!");
+      res.redirect("/ideas");
+    });
+  });
+});
+
+// Delete Idea
+app.delete("/ideas/:id", (req, res) => {
+  Idea.deleteOne({
+    _id: req.params.id,
+  }).then((idea) => {
+    req.flash("success_msg", "Video idea removed!");
+    res.redirect("/ideas");
+  });
 });
 
 // About page
